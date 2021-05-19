72a1e368ecc1f9163a06d32d46bd69556fbeced9
lingsiyun <lanxuecao@qq.com>
2021-05-19 20:45:04 +0800

    注释

--- github.py
+++ github.py
@@ -21,7 +21,7 @@ while True:
             content = f.read()        
             f.seek(0, 0)
             f.write(p+'\n'+content)
-        b=os.popen("git add --all && git commit -m '注释' && git push").read()
+        b=os.popen("cd /home/pi/github/lingsiyun.github.io && git add --all && git commit -m '注释' && git push").read()
         print(b)
 
     
