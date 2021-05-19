af636ef94e8855c4c52f3801f5626da6e58bf121
lingsiyun <lanxuecao@qq.com>
2021-05-19 20:42:03 +0800

    注释

--- github.py
+++ github.py
@@ -13,7 +13,7 @@ while True:
     t=t+1
     p=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     print(p)
-    if t==60:
+    if t==6:
         
         print(t)
         t=0
@@ -21,7 +21,7 @@ while True:
             content = f.read()        
             f.seek(0, 0)
             f.write(p+'\n'+content)
-        b=os.popen("cd /home/pi/github/lingsiyun.github.io && git add --all && git commit -m '注释' && git push").read()
+        b=os.popen("git add --all && git commit -m '注释' && git push").read()
         print(b)
 
     
